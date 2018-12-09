cdbd9725276bd278b2ab2f1416530c7e1d01dd95
Torbjörn Hansson <toha73@gmail.com>
2018-12-09 19:00:06 +0100

    Day 9

---
+++ src/AoC2018/Day9.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Advent_of_Code_2018
+{
+    public class Day9
+    {
+        public static long Part1And2(int players, int lastMarbleWorth)
+        {
+            var marbles = new LinkedList<int>();
+            var currentMarble = marbles.AddFirst(0);
+            var playerScores = Enumerable.Range(1, players).Select(i => (long)0).ToList();
+            foreach (var marble in Enumerable.Range(1, lastMarbleWorth))
+            {
+                if (marble % 23 == 0)
+                {
+                    var player = marble % players;
+                    currentMarble = currentMarble.Backward(7);
+                    playerScores[player] += marble + (long)currentMarble.Value;
+                    currentMarble = currentMarble.Forward(1);
+                    currentMarble.List.Remove(currentMarble.Backward(1));
+                }
+                else
+                {
+                    currentMarble = currentMarble.Forward(1);
+                    currentMarble = currentMarble.List.AddAfter(currentMarble, marble);
+                }
+            }
+            return playerScores.Max();
+        }
+    }
+
+    public static class Extensions
+    {
+        public static LinkedListNode<T> Forward<T>(this LinkedListNode<T> node, int count)
+        {
+            if (count == 0) return node;
+            var n = node;
+            foreach (var i in Enumerable.Range(0, count))
+            {
+                n = n.Next ?? n.List.First;
+            }
+            return n;
+        }
+        public static LinkedListNode<T> Backward<T>(this LinkedListNode<T> node, int count)
+        {
+            if (count == 0) return node;
+            var n = node;
+            if (count > 0)
+            {
+                foreach (var i in Enumerable.Range(0, count))
+                {
+                    n = n.Previous ?? n.List.Last;
+                }
+            }
+            return n;
+        }
+    }
+}
---
+++ test/AoC2018.Test/Day9Test.cs
@@ -0,0 +1,35 @@
+﻿using NUnit.Framework;
+
+namespace Advent_of_Code_2018.Test
+{
+    public class Day9Test
+    {
+        [Test]
+        public void Part1_simple_tests()
+        {
+            Assert.Multiple(() =>
+            {
+                Assert.That(Day9.Part1And2(9, 25), Is.EqualTo(32));
+                Assert.That(Day9.Part1And2(10, 1618), Is.EqualTo(8317));
+                Assert.That(Day9.Part1And2(13, 7999), Is.EqualTo(146373));
+                Assert.That(Day9.Part1And2(17, 1104), Is.EqualTo(2764));
+                Assert.That(Day9.Part1And2(21, 6111), Is.EqualTo(54718));
+                Assert.That(Day9.Part1And2(30, 5807), Is.EqualTo(37305));
+            });
+        }
+
+        [Test]
+        public void Part1_real_test()
+        {
+            Assert.That(Day9.Part1And2(423, 71944), Is.EqualTo(418237));
+        }
+
+        [Test]
+        public void Part2_real_test()
+        {
+            Assert.That(Day9.Part1And2(423, 100 * 71944), Is.EqualTo(3505711612));
+        }
+
+
+    }
+}
