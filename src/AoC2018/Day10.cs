69b454872214da2cc9b13864db44c2d9ebc921c4
Torbjörn Hansson <toha73@gmail.com>
2019-01-25 19:08:46 +0100

    Added day 10.

---
+++ src/AoC2018/Day10.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Advent_of_Code_2018
+{
+    public class Day10
+    {
+        public static Bitmap Part1(string input)
+        {
+            var points = ParseInput(input);
+
+            MovePoints(ref points);
+
+            return Paint(points);
+        }
+
+        public static int Part2(string input)
+        {
+            var points = ParseInput(input);
+
+            return MovePoints(ref points);
+        }
+
+        public static int MovePoints(ref List<Point> points)
+        {
+            foreach (var iteration in Enumerable.Range(1, 100000))
+            {
+                foreach (var point in points)
+                {
+                    point.Position += point.Velocity;
+                }
+
+                if (AreAllAdjacentToOthers(points))
+                {
+                    Paint(points);
+                    return iteration;
+                }
+            }
+            throw new Exception("Nope!");
+        }
+
+
+        public static Bitmap Paint(List<Point> points)
+        {
+            var zoom = 1f;
+            const int padding = 4;
+            var left = points.Min(p => p.Position.X);
+            var width = points.Max(p => p.Position.X) - left;
+            var top = points.Min(p => p.Position.Y);
+            var height = points.Max(p => p.Position.Y) - top;
+
+            var bitmap = new Bitmap((int)(width * zoom) + 1 + padding, (int)(height * zoom) + 1 + padding);
+            foreach (var point in points)
+            {
+                bitmap.SetPixel(
+                    (int)((point.Position.X - left) * zoom) + padding / 2,
+                    (int)((point.Position.Y - top) * zoom) + padding / 2,
+                    Color.Red);
+            }
+            return bitmap;
+        }
+
+        private static bool AreAllAdjacentToOthers(List<Point> points)
+        {
+            var checkedPositions = new HashSet<Position>();
+            for (var i = 0; i < points.Count - 1; i++)
+            {
+                if (checkedPositions.Contains(points[i].Position)) continue;
+
+                var adjacentPoints = points.Skip(i + 1).Where(p => p.IsAdjacentTo(points[i])).ToList();
+                if (adjacentPoints.Count == 0)
+                {
+                    return false;
+                }
+                checkedPositions.Add(points[i].Position);
+                checkedPositions.UnionWith(adjacentPoints.Select(p => p.Position));
+            }
+            return true;
+        }
+
+        public static List<Point> ParseInput(string input)
+        {
+            return input.Split(new[] { '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(l => Regex.Match(l, @"position=<\s*?(-?\d*),\s*?(-?\d*)> velocity=<\s*?(-?\d*),\s*?(-?\d*)>"))
+                .Select(m => new Point
+                {
+                    Position = new Position(int.Parse(m.Groups[1].Value), int.Parse(m.Groups[2].Value)),
+                    Velocity = new Position(int.Parse(m.Groups[3].Value), int.Parse(m.Groups[4].Value)),
+                })
+                .ToList();
+        }
+
+
+
+        public class Point
+        {
+            public Position Position { get; set; }
+            public Position Velocity { get; set; }
+            public bool IsAdjacentTo(Point p) => p.Position.IsAdjacentTo(Position);
+            public override string ToString() => Position.ToString();
+        }
+
+        public struct Position
+        {
+            public Position(int x, int y)
+            {
+                X = x; Y = y;
+            }
+            public int X { get; set; }
+            public int Y { get; set; }
+            public bool IsEmpty => X == 0 && Y == 0;
+            public override string ToString() => $"<{X},{Y}>";
+            public bool IsAdjacentTo(Position p)
+            {
+                var d = this - p;
+                return !d.IsEmpty
+                    && d.X <= 1 && d.X >= -1
+                    && d.Y <= 1 && d.Y >= -1;
+            }
+            public static Position operator +(Position p1, Position p2) => new Position { X = p1.X + p2.X, Y = p1.Y + p2.Y };
+            public static Position operator -(Position p1, Position p2) => new Position { X = p1.X - p2.X, Y = p1.Y - p2.Y };
+        }
+    }
+}
---
+++ test/AoC2018.Test/Day10Test.cs
@@ -0,0 +1,82 @@
+﻿using Advent_of_Code_2018.Test.Helpers;
+using NUnit.Framework;
+
+namespace Advent_of_Code_2018.Test
+{
+    public class Day10Test
+    {
+        private string _input;
+
+        [SetUp]
+        public void BeforeEach()
+        {
+            _input = ResourceHelper.GetInputStringAocDay(typeof(Day10));
+        }
+
+
+        [Test]
+        public void Part1_simple_test()
+        {
+            var result = Day10.Part1(_simpleInput);
+
+            Assert.Pass("Bitmap with text: HI");
+        }
+
+
+        [Test]
+        public void Part1_real_test()
+        {
+            var result = Day10.Part1(_input);
+
+            Assert.Pass("Bitmap with text: XECXBPZB");
+        }
+
+
+        [Test]
+        public void Part2_simple_test()
+        {
+            Assert.That(Day10.Part2(_simpleInput), Is.EqualTo(3));
+        }
+
+
+        [Test]
+        public void Part2_real_test()
+        {
+            Assert.That(Day10.Part2(_input), Is.EqualTo(10124));
+        }
+
+        private const string _simpleInput = @"
+position=< 9,  1> velocity=< 0,  2>
+position=< 7,  0> velocity=<-1,  0>
+position=< 3, -2> velocity=<-1,  1>
+position=< 6, 10> velocity=<-2, -1>
+position=< 2, -4> velocity=< 2,  2>
+position=<-6, 10> velocity=< 2, -2>
+position=< 1,  8> velocity=< 1, -1>
+position=< 1,  7> velocity=< 1,  0>
+position=<-3, 11> velocity=< 1, -2>
+position=< 7,  6> velocity=<-1, -1>
+position=<-2,  3> velocity=< 1,  0>
+position=<-4,  3> velocity=< 2,  0>
+position=<10, -3> velocity=<-1,  1>
+position=< 5, 11> velocity=< 1, -2>
+position=< 4,  7> velocity=< 0, -1>
+position=< 8, -2> velocity=< 0,  1>
+position=<15,  0> velocity=<-2,  0>
+position=< 1,  6> velocity=< 1,  0>
+position=< 8,  9> velocity=< 0, -1>
+position=< 3,  3> velocity=<-1,  1>
+position=< 0,  5> velocity=< 0, -1>
+position=<-2,  2> velocity=< 2,  0>
+position=< 5, -2> velocity=< 1,  2>
+position=< 1,  4> velocity=< 2,  1>
+position=<-2,  7> velocity=< 2, -2>
+position=< 3,  6> velocity=<-1, -1>
+position=< 5,  0> velocity=< 1,  0>
+position=<-6,  0> velocity=< 2,  0>
+position=< 5,  9> velocity=< 1, -2>
+position=<14,  7> velocity=<-2,  0>
+position=<-3,  6> velocity=< 2, -1>
+";
+    }
+}
