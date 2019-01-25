819a4b82fe0defea6fb61b401f76af40e617b4d6
Torbjörn Hansson <toha73@gmail.com>
2019-01-25 19:19:02 +0100

    Added Day11

---
+++ src/AoC2018/Day11.cs
@@ -0,0 +1,114 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Advent_of_Code_2018
+{
+    public class Day11
+    {
+        public static string Part1(int serialNumber)
+        {
+            var grid = GetGrid(serialNumber);
+
+            var result = grid.GetSquareMaxResult(3);
+
+            return $"{result.X},{result.Y}";
+        }
+
+        public static string Part2(int serialNumber)
+        {
+            var grid = GetGrid(serialNumber);
+
+            foreach (var i in Enumerable.Range(2, 300))
+            {
+                grid.CalculatePowerGridLevels(i);
+            }
+
+            var top = grid.MaxResults.OrderByDescending(r => r.Value.Level).Take(10);
+            var first = top.First();
+
+            return $"{first.Value.X},{first.Value.Y},{first.Key}";
+        }
+
+        public static int GetHighestSquarePowerLevel(int serialNumber)
+        {
+            var grid = GetGrid(serialNumber);
+
+            return grid.GetSquareMaxResult(3).Level;
+        }
+
+        private static Grid GetGrid(int serialNumber)
+        {
+            var cells = Enumerable.Range(0, 300 * 300)
+                .Select(i => (i % 300 + 1, i / 300 + 1))
+                .ToDictionary(f => f, f => new FuelCell { Position = f, PowerLevel = GetPowerLevel(f, serialNumber) });
+
+            return new Grid { FuelCells = cells };
+        }
+
+        public static int GetPowerLevel((int X, int Y) position, int serialNumber)
+        {
+            var rackId = position.X + 10;
+            var p = (rackId * position.Y + serialNumber) * rackId;
+            var z = p > 100
+                ? p / 100 % 10
+                : 0;
+            return z - 5;
+        }
+
+        public class Grid
+        {
+            public Dictionary<(int X, int Y), FuelCell> FuelCells { get; set; }
+            public Dictionary<int, MaxResult> MaxResults { get; set; } = new Dictionary<int, MaxResult>();
+
+            public void CalculatePowerGridLevels(int squareSize)
+            {
+                var columnAmount = new Dictionary<(int, int), int>();   // column starting at position and squareSize height.
+
+                if (MaxResults.ContainsKey(squareSize)) return;
+                var maxResult = new MaxResult();
+                foreach (var y in Enumerable.Range(1, 301 - squareSize))
+                {
+                    columnAmount.Clear();
+                    foreach (var x in Enumerable.Range(1, 300))
+                    {
+                        var columnValue = Enumerable.Range(y, squareSize).Sum(y2 => FuelCells[(x, y2)].PowerLevel);
+                        columnAmount[(x, y)] = columnValue;
+                    }
+                    foreach (var x in Enumerable.Range(1, 301 - squareSize))
+                    {
+                        var level = Enumerable.Range(x, squareSize).Sum(x2 => columnAmount[(x2, y)]);
+                        if (level > maxResult.Level)
+                        {
+                            maxResult = new MaxResult { X = x, Y = y, Level = level };
+                        }
+                    }
+                }
+                MaxResults.Add(squareSize, maxResult);
+            }
+
+
+            public MaxResult GetSquareMaxResult(int squareSize)
+            {
+                CalculatePowerGridLevels(squareSize);
+
+                return MaxResults[squareSize];
+            }
+        }
+
+        public struct MaxResult
+        {
+            public int X;
+            public int Y;
+            public int Level;
+        }
+
+        public class FuelCell
+        {
+            public (int X, int Y) Position { get; set; }
+            public int PowerLevel { get; set; }
+            public override string ToString() => Position.ToString();
+            public static FuelCell Create(int x, int y) => new FuelCell { Position = (x, y) };
+            public static FuelCell Create((int x, int y) position) => new FuelCell { Position = position };
+        }
+    }
+}
---
+++ test/AoC2018.Test/Day11Test.cs
@@ -0,0 +1,43 @@
+﻿using NUnit.Framework;
+
+namespace Advent_of_Code_2018.Test
+{
+    public class Day11Test
+    {
+        [Test]
+        [TestCase(3, 5, 8, 4)]
+        [TestCase(122, 79, 57, -5)]
+        [TestCase(217, 196, 39, 0)]
+        [TestCase(101, 153, 71, 4)]
+        public void Part1_getpowerlevel_should_return_correct_result(int x, int y, int serialNumber, int expectedResult)
+        {
+            Assert.That(Day11.GetPowerLevel((x, y), serialNumber), Is.EqualTo(expectedResult));
+
+            Assert.That(Day11.GetHighestSquarePowerLevel(18), Is.EqualTo(29));
+            Assert.That(Day11.GetHighestSquarePowerLevel(42), Is.EqualTo(30));
+        }
+
+        [Test]
+        [TestCase(18, 29)]
+        [TestCase(42, 30)]
+        public void Part1_gethighestSquarePowerLevel_should_return_correct_result(int serialNumber, int expectedResult)
+        {
+            Assert.That(Day11.GetHighestSquarePowerLevel(serialNumber), Is.EqualTo(expectedResult));
+        }
+
+
+        [Test]
+        public void Part1_real_test()
+        {
+            Assert.That(Day11.Part1(7689), Is.EqualTo("20,37"));
+        }
+
+        [Test]
+        [Ignore("Takes ~4 minutes")]
+        public void Part2_real_test()
+        {
+            // Takes ~4 min. :(
+            Assert.That(Day11.Part2(7689), Is.EqualTo("90,169,15"));
+        }
+    }
+}
